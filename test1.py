b0dfec718e5e99cbd9fb835d90b4e99aadb55263
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2015-05-25 01:29:50 -0700

    include realnames

--- test1.py
+++ test1.py
@@ -2,31 +2,44 @@ import flickr_api as f
 import sys
 import re
 
+names = True
 
 u = f.Person.findByUserName('What Photos Look Like')
 
 badges = {}
 for p in u.getPhotos(tags=['liquidity'],per_page=400):
     n = 0 
+    print '--- %s -------' % (p.title)
     for c in p.getComments():
-        m = re.search(r'<img.*src="([^"]*)[^>]*>',c.text)
+        m = re.search(r'(.*)(<img.*src="([^"]*)[^>]*>)(.*)',c.text)
         if m is not None:
             n += 1
-            k = m.group(1)
-            q = re.search(r'<img.*alt="([^"]*)[^>]*>',m.group(0))
-            if q is not None:
-                k = 'Alt:%s' % (q.group(1))
+            if names:
+                k = c.realname
             else:
-                q = re.search(r'/([^/]*)\.jpg',m.group(1))
+                k = m.group(3)
+                q = re.search(r'<img.*alt="([^"]*)[^>]*>',m.group(2))
                 if q is not None:
-                    k = q.group(1)
+                    k = 'Alt:%s' % (q.group(1))
+                else:
+                    q = re.search(r'/([^/]*)\.jpg',m.group(3))
+                    if q is not None:
+                        k = q.group(1)
             badges[k] = 1 + badges.get(k,0)
+            #try:
+            #    print ":: %s<%s>%s"%(m.group(1),k,m.group(4))
+            #except:
+            #    print "oops"
+            # print dir(c)
+
             #try:
             #    print c.text
             #except:
             #    pass
     #if n > 0:
     #    print "%s: %d picture comments" % (p.title,n)
+
+# print dir(c)
 
 t = 0
 for b in badges:
